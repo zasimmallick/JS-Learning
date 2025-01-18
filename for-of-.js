22269b0847fbdbb1348f0225bea424d19f62c3b3
zasimmallick <zasimmallickofficial@gmail.com>
2025-01-19 00:23:16 +0600

    js loop add

---
+++ for-of-.js
@@ -0,0 +1,11 @@
+const numbers = [3 , 11, 13, 29, 6];
+
+console.log(numbers);
+
+for(const num of numbers){
+  console.log(num);
+}
+
+// for() {
+
+// }
---
+++ while.js
@@ -0,0 +1,20 @@
+// const date = 1
+
+// if (date == 13){
+//     console.log("my barth date")
+// }
+
+// console.log("not my barth date")
+
+
+// wile loop
+
+
+let num = 0;
+
+while (num < 5){
+    console.log("looping", num )
+//    num = num + 1
+// num += 1;
+num++
+}

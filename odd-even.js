e442a7f47fb8c8c1d50f71dc88be99084f1333bf
zasimmallick <zasimmallickofficial@gmail.com>
2025-01-19 21:10:08 +0600

    loop add

---
+++ odd-even.js
@@ -0,0 +1,21 @@
+// for(let i = 1; i <= 50; i++ ){
+//     console.log(i);
+// }
+
+// // even
+// for(let i = 0; i <= 20; i=i+2 ){
+//     console.log(i);
+// }
+// // odd
+// for(let i = 1; i <= 20; i=i+2 ){
+//     console.log(i);
+// }
+
+// sum
+let sum = 0
+
+for(let i = 11; i <= 20; i++ ){
+    sum = sum + i
+    console.log(i);
+}
+console.log("sum of numbers", sum)

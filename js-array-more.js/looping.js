3e603c6f1447defaadb73aa819068cac4484726d
zasimmallick <zasimmallickofficial@gmail.com>
2025-01-21 03:00:14 +0600

    object add

---
+++ js-array-more.js/looping.js
@@ -0,0 +1,31 @@
+// let languages = ["English", "Bengali", "Urdu", "Arabic"];
+
+// for (let i = 0; i < languages.length; i++) {
+//     // console.log(languages[i]);
+// }
+
+
+
+// let languages = ["English", "Bengali", "Urdu", "Arabic"];
+// let i = 0;
+
+// while (i < languages.length) {
+//     console.log(languages[i]);
+//     i++; // Increment the index
+// }
+
+
+// let languages = ["English", "Bengali", "Urdu", "Arabic"];
+
+// for (let i = languages.length - 1; i >= 0; i--) {
+//     console.log(languages[i]);
+// }
+
+// let languages = ["English", "Bengali", "Urdu", "Arabic"];
+// let i = languages.length - 1;
+
+// while (i >= 0) {
+//     console.log(languages[i]);
+//     i--; // Decrement the index
+// }
+
---
+++ js-array-more.js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let languages = ["English", "Bengali", "Urdu", "Arabic"];
+
+// for (let i = 0; i < languages.length; i++) {
+//     // console.log(languages[i]);
+// }
+
